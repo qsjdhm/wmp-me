f09fc92aac0a1beb23e163e0aa68859db5b76b25
qsjdhm <qsjdhm@163.com>
2020-04-20 19:58:55 +0800

    集成colorui

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App'
 
+import accumulate from './pages/accumulate/index.vue'
+Vue.component('accumulate',accumulate)
+
+import profile from './pages/profile/index.vue'
+Vue.component('profile',profile)
+
+import basics from './pages/basics/home.vue'
+Vue.component('basics',basics)
+
+import components from './pages/component/home.vue'
+Vue.component('components',components)
+
+import plugin from './pages/plugin/home.vue'
+Vue.component('plugin',plugin)
+
+import cuCustom from './colorui/components/cu-custom.vue'
+Vue.component('cu-custom',cuCustom)
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,41 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+        <accumulate v-if="PageCur=='accumulate'"></accumulate>
+        <profile v-if="PageCur=='profile'"></profile>
+		<basics v-if="PageCur=='basics'"></basics>
+		<components v-if="PageCur=='component'"></components>
+		<plugin v-if="PageCur=='plugin'"></plugin>
+		<view class="cu-bar tabbar bg-white shadow foot">
+            <view class="action" @click="NavChange" data-cur="accumulate">
+            	<view class='cuIcon-cu-image'>
+            		<image :src="'/static/tabbar/basics' + [PageCur=='accumulate'?'_cur':''] + '.png'"></image>
+            	</view>
+            	<view :class="PageCur=='accumulate'?'text-green':'text-gray'">积累</view>
+            </view>
+            <view class="action" @click="NavChange" data-cur="profile">
+            	<view class='cuIcon-cu-image'>
+            		<image :src="'/static/tabbar/basics' + [PageCur=='profile'?'_cur':''] + '.png'"></image>
+            	</view>
+            	<view :class="PageCur=='profile'?'text-green':'text-gray'">资料</view>
+            </view>
+			<view class="action" @click="NavChange" data-cur="basics">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/basics' + [PageCur=='basics'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='basics'?'text-green':'text-gray'">元素</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="component">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/component' + [PageCur == 'component'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='component'?'text-green':'text-gray'">组件</view>
+			</view>
+			<view class="action" @click="NavChange" data-cur="plugin">
+				<view class='cuIcon-cu-image'>
+					<image :src="'/static/tabbar/plugin' + [PageCur == 'plugin'?'_cur':''] + '.png'"></image>
+				</view>
+				<view :class="PageCur=='plugin'?'text-green':'text-gray'">扩展</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -10,43 +43,18 @@
 <script>
 	export default {
 		data() {
-			return {
-				title: 'Hello'
+		return {
+				PageCur: 'profile'
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
