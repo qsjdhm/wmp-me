5075c730e545f79891ad5929220f5674b0c4175e
qsjdhm <qsjdhm@163.com>
2020-04-21 20:17:54 +0800

    阿烽

--- pages/accumulate/index.vue
+++ pages/accumulate/index.vue
@@ -11,26 +11,20 @@
 				</navigator>
 			</view>
 			
-			<view class="cu-card case" :class="isCard?'no-card':''">
-				<view class="cu-item shadow">
-					<view class="image">
-						<image src="https://cdn.nlark.com/yuque/0/2020/jpeg/634279/1584537870665-e024d91b-793f-4983-a042-f2eba3ee7613.jpeg?x-oss-process=image/resize,w_1232"
-						 mode="widthFix"></image>
-						<view class="cu-bar bg-shadeBottom"> <text class="text-cut">我已天理为凭，踏入这片荒芜，不再受凡人的枷锁遏制。我已天理为凭，踏入这片荒芜，不再受凡人的枷锁遏制。</text></view>
-					</view>
+			<view class="cu-card case">
+				<view v-for="(item,index) in articles" :key="index" class="cu-item shadow">
+					<view class="image" v-bind:style="{backgroundImage:'url(' + item.cover + ')'}"></view>
 					<view class="cu-list menu-avatar">
 						<view class="cu-item">
-							<view class="cu-avatar round lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10006.jpg);"></view>
+							<view class="cu-avatar round lg" style="background-image:url(https://cdn.nlark.com/yuque/0/2019/png/634279/1575884363463-avatar/568dd23a-43e8-423c-b250-ea592cdd597e.png?x-oss-process=image%2Fresize%2Cm_fill%2Cw_48%2Ch_48%2Fformat%2Cpng);"></view>
 							<view class="content flex-sub">
-								<view class="text-grey">正义天使 凯尔</view>
-								<view class="text-gray text-sm flex justify-between">
-									十天前
-								</view>
+								<view class="text-grey">{{item.title}}</view>
 							</view>
 						</view>
 					</view>
 				</view>
 			</view>
+            
 			
 			<view class="cu-tabbar-height"></view>
 		</scroll-view>
@@ -42,7 +36,8 @@
 		name: 'accumulate',
 		data() {
 			return {
-				elements: [{
+				elements: [
+                    {
 						title: '资产库知识体系',
 						name: 'Asset Library',
 						color: 'cyan'
@@ -53,6 +48,34 @@
 						color: 'blue'
 					}
 				],
+                articles: [
+                    {
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1586497733695-2b2701df-d42f-4deb-9c5f-c978cec9c182.png?x-oss-process=image/resize,w_746',
+                        title: '【06】引入成熟的权限整体方案-上（完成）'
+                    },
+                    {
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1584761224769-4e64c3f7-79f3-43ba-bc17-783610c8dfe2.png?x-oss-process=image/resize,w_746',
+                        title: '【05】建立文档化服务（完成）'
+                    },
+                    {
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1584670791240-a85c0a7c-b1bc-4154-9a16-5cfb59a8fa07.png?x-oss-process=image/resize,w_746',
+                        title: '【04】引入VT UI 2.0设计元素（完成）'
+                    },
+                    {
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1584595362555-d6c0f8af-a445-470e-b99c-9235bd9b1314.png?x-oss-process=image/resize,w_746',
+                        title: '【03】图标方案改进（完成）'
+                    },
+                    {
+                        
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1584587188841-71bc570e-6500-4d00-a114-b02b90f10cd4.png?x-oss-process=image/resize,w_746',
+                        title: '【02】克隆vue-element-admin源码并去除多余结构（完成）'
+                    },
+                    {
+                        cover: 'https://cdn.nlark.com/yuque/0/2020/png/634279/1587468527868-5fbb0f15-89a9-4b33-9726-b04e7612e550.png?x-oss-process=image/resize,w_746',
+                        title: '【01】讲解前端技术向中台代码库概念（完成）'
+                        
+                    }
+                ]
 			}
 		},
 		onLoad() {
@@ -69,15 +92,33 @@
 		height: 100vh;
 	}
 	
-	.accumulate-page .cu-card>.cu-item {
-		background-color: #ffffff00!important;
-		margin: 20rpx 40rpx;
-		padding: 2.5%;
+	.accumulate-page .cu-card>.cu-item { 
+		background-color: transparent!important;
+		margin: 30rpx 40rpx;
+		padding: 0 2.5%;
 	}
+    
+    .accumulate-page .image {
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        height: 320rpx!important;
+        border-top-left-radius: 15rpx;
+        border-top-right-radius: 15rpx;
+    }
 	
-	.accumulate-page .cu-card>.cu-item image {
-		border-radius: 15rpx;
-	}
+    .accumulate-page .cu-card>.cu-item .cu-list {
+        border-bottom-left-radius: 15rpx;
+        border-bottom-right-radius: 15rpx;
+    }
+    
+    .accumulate-page .cu-card>.cu-item .content {
+        display: block!important;
+    }
+    
+    .accumulate-page .cu-list.menu-avatar>.cu-item {
+        border-top: 1rpx solid #eee;
+    }
 	
 
 </style>
