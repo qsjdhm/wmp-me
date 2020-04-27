7b73052eb6f23c0c7491d0bf8b67669aed7284d4
张岩 <qsjdhm@163.com>
2020-04-27 21:42:26 +0800

    no message

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,10 +1,59 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">me {{title}}</text>
+	<view>
+		<cu-custom bgColor="bg-gradual-blue" :isBack="true"><block slot="backText">返回</block><block slot="content">来撩我吧</block></cu-custom>
+		<view class="profile-pack">
+			<view class="basics-pack">
+				<view class="cu-avatar xl round" style="background-image:url(https://cdn.nlark.com/yuque/0/2019/png/634279/1575884363463-avatar/568dd23a-43e8-423c-b250-ea592cdd597e.png?x-oss-process=image%2Fresize%2Cm_fill%2Cw_48%2Ch_48%2Fformat%2Cpng);"></view>
+				<view class="info-pack">
+					<view class="name">不拽注定被甩~</view>
+					<view class="position">前端leader</view>
+				</view>
+			</view>
+			<view class="cu-timeline">
+				<view class="cu-time">目前我在做什么</view>
+				<view class="cu-item">
+					<view class="content">
+						<view class="cu-capsule radius">
+							<view class="cu-tag bg-cyan">上午</view>
+							<view class="cu-tag line-cyan">10:00</view>
+						</view>
+						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+					</view>
+				</view>
+				<view class="cu-time">对前端leader的理解</view>
+				<view class="cu-item">
+					<view class="content">
+						<view class="cu-capsule radius">
+							<view class="cu-tag bg-cyan">上午</view>
+							<view class="cu-tag line-cyan">10:00</view>
+						</view>
+						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+					</view>
+				</view>
+				<view class="cu-time">关注的技术点</view>
+				<view class="cu-item">
+					<view class="content">
+						<view class="cu-capsule radius">
+							<view class="cu-tag bg-cyan">上午</view>
+							<view class="cu-tag line-cyan">10:00</view>
+						</view>
+						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+					</view>
+				</view>
+				<view class="cu-time">对未来的目标</view>
+				<view class="cu-item">
+					<view class="content">
+						<view class="cu-capsule radius">
+							<view class="cu-tag bg-cyan">上午</view>
+							<view class="cu-tag line-cyan">10:00</view>
+						</view>
+						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
+	
 </template>
 
 <script>
@@ -24,6 +73,51 @@
 </script>
 
 <style>
+	.profile-pack {
+		padding: 30upx;
+		width: 100%;
+		height: 100%;
+		background-color: #fff;
+	}
+	.basics-pack {
+		display: -webkit-flex;
+		display: flex;
+		margin-bottom: 30upx;
+	}
+	.basics-pack .cu-avatar {
+		width: 128upx;
+		height: 128upx;
+	}
+	.basics-pack .info-pack {
+		margin-left: 20upx;
+		flex: 1;
+	}
+	.basics-pack .info-pack .name {
+		height: 74upx;
+		margin-bottom: 10upx;
+		display: -webkit-flex;
+		display: flex;
+		align-items: flex-end;
+		font-size: 36upx;
+		color: #333;
+	}
+	.basics-pack .info-pack .position {
+		height: 39upx;
+		margin-top: 5upx;
+		display: -webkit-flex;
+		display: flex;
+		align-items: flex-start;
+		color: #888;
+		font-size: 26upx;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	.content {
 		display: flex;
 		flex-direction: column;
