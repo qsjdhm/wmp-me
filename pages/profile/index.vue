d2ea28c69ea4a94feffdccd3b669a5890fdabae4
qsjdhm <qsjdhm@163.com>
2020-05-14 19:36:04 +0800

    no message

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -20,7 +20,8 @@
 							<view class="cu-tag bg-blue">微前端</view>
                             <view class="cu-tag line-blue">低代码开发</view>
 						</view>
-						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+						<view class="margin-top">微前端：目前市面上的微前端概念可落地的是前端容器化（iframe）和应用组件化（Web Components），正在研究它也是为了解决其他公司发展中会遇到的问题。React在这里有天生优势，但我们用的是Vue，只能自己探索</view>
+                        <view class="margin-top">低代码开发：为了帮助公司减少开发成本，正在落地低代码开发方式（自动化、模板化、组件化、开发规范），从效果来看确实有显著成效</view>
 					</view>
 				</view>
                 <view class="cu-time">关注的点</view>
@@ -30,7 +31,9 @@
                 			<view class="cu-tag bg-blue">前端架构</view>
                 			<view class="cu-tag line-blue">团队管理</view>
                 		</view>
-                		<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+                        <view class="margin-top">前端架构：做技术架构没有捷径，只能多做CodeReview、多听别人分享、多花时间学习</view>
+                        <view class="margin-top">团队管理：对下级要技术上有氛围、制度上更透明；对上级要及时发现问题、解决问题；对平级要互通、主动沟通</view>
+                        
                 	</view>
                 </view>
 				<view class="cu-time">对前端leader的理解</view>
@@ -40,7 +43,8 @@
 							<view class="cu-tag bg-blue">解决问题</view>
 							<view class="cu-tag line-blue">拿结果</view>
 						</view>
-						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+						<view class="margin-top">向上：可以给老板一种我能解决问题、并且有及时反馈的效果，需要主动发现老板头疼的问题并且主动解决问题</view>
+						<view class="margin-top">向下：团队建立需达到同舟共济、价值共识、开心工作、目标清晰、使命必达、组员能获取到结果</view>
 					</view>
 				</view>
 				<view class="cu-time">对职业的目标</view>
@@ -50,7 +54,9 @@
 							<view class="cu-tag bg-blue">提高视野</view>
 							<view class="cu-tag line-blue">组圈子做事情</view>
 						</view>
-						<view class="margin-top">这是第一次，我家的铲屎官走了这么久。久到足足有三天！！ 在听到他的脚步声响在楼梯间的那一刻，我简直想要破门而出，对着他狠狠地吼上10分钟，然后再看心情要不要他进门。</view>
+                        <view class="margin-top">提高视野：需要看透某些业务、技术的终局是什么，未来方向又是什么</view>
+                        <view class="margin-top">转变思维：从业务的角度看整个业务线缺少哪块、业务需要技术实现哪些东西、打通技术与业务之间的通道</view>
+                        <view class="margin-top">组圈子：希望可以协调和积累更多人力、技术、资源，逐步形成小圈子做事情效果</view>
 					</view>
 				</view>
 			</view>
